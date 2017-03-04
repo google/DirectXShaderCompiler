e0ef1f75d5495627872c95f40a3855fad01c7a23
Lei Zhang <antiagainst@google.com>
2017-03-04 15:41:17 -0500

    Add basic SPIR-V builder functionalities to dump module header

--- tools/clang/include/clang/SPIRV/SPIRVBuilder.h
+++ tools/clang/include/clang/SPIRV/SPIRVBuilder.h
@@ -9,15 +9,33 @@
 #ifndef LLVM_CLANG_SPIRV_SPIRVBUILDER_H
 #define LLVM_CLANG_SPIRV_SPIRVBUILDER_H
 
+#include <vector>
+
 namespace clang {
 namespace spirv {
 
 class SPIRVBuilder {
 public:
-  SPIRVBuilder() : NextID(0) {}
+  SPIRVBuilder();
+
+  /// \brief Consumes the next unused result id.
+  inline uint32_t TakeNextID();
+
+  /// \brief Begins building a SPIR-V module.
+  void BeginModule();
+  /// \brief Ends building the current module.
+  void EndModule();
+  /// \brief Takes the SPIR-V module under building.
+  std::vector<uint32_t> TakeModule();
 
 private:
+  /// \brief Generates a header into the SPIR-V module under building.
+  void GenHeader();
+
+  /// \brief The next unused result id.
   uint32_t NextID;
+  /// \brief The module under building.
+  std::vector<uint32_t> TheModule;
 };
 
 } // end namespace spirv
