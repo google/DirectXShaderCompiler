b1623a132e4069e564d04c486844c685b2bdcb3f
Ehsan <ehsann@google.com>
2017-03-24 17:16:40 -0400

    Add googletest & more! (#11)
    * Added googletest.
    * Added "-spirv" to hctbuild.
    * Added "spirv" and "spirv_only" targets for hcttest.
    * Added a sample unit test for the ModuleBuilder class.
    
    Running `hctbuild` does *not* build googletest and SPIRV unit tests.
    Running `hcttest` does *not* run SPIRV unit tests.
    
    Running `hctbuild -spirv` does build googletest and SPIRV unit tests.
    Running `hcttest spirv` will run ALL tests including SPIRV unit tests.
    Running `hcttest spirv_only` will ONLY run SPIRV unit tests.
    
    In short, the default behavior of hctbuild and hcttest is not changed.

--- utils/hct/hcttest.cmd
+++ utils/hct/hcttest.cmd
@@ -13,6 +13,11 @@ set TEST_EXTRAS=1
 rem Whether we built the project using ninja as the generator.
 set GENERATOR_NINJA=0
 
+rem Begin SPIRV change
+set TEST_SPIRV=0
+set TEST_SPIRV_ONLY=0
+rem End SPIRV change
+
 if "%BUILD_CONFIG%"=="" (
   set BUILD_CONFIG=Debug
 )
@@ -25,6 +30,19 @@ if "%1"=="clean" (
   set TEST_EXTRAS=0
   shift /1
 )
+
+rem Begin SPIRV change
+if "%1"=="spirv" (
+  set TEST_SPIRV=1
+  shift /1
+)
+
+if "%1"=="spirv_only" (
+  set TEST_SPIRV=1
+  set TEST_SPIRV_ONLY=1
+  shift /1
+)
+rem End SPIRV change
 
 if "%1"=="clang" (
   set TEST_EXEC=0
@@ -111,6 +129,24 @@ if "%TEST_CLEAN%"=="1" (
   rmdir /q /s %TEST_DIR%
   exit /b 0
 )
+
+rem Begin SPIRV change
+if "%TEST_SPIRV%"=="1" (
+  if not exist %HLSL_BLD_DIR%\tools\clang\unittests\SPIRV\%BUILD_CONFIG%\clang-spirv-tests.exe (
+    echo clang-spirv-tests.exe has not been built. Make sure you run "hctbuild -spirv" first.
+    exit /b 1
+  )
+  echo Running SPIRV tests ...
+  %HLSL_BLD_DIR%\tools\clang\unittests\SPIRV\%BUILD_CONFIG%\clang-spirv-tests.exe
+  if errorlevel 1 (
+    echo Failure occured in SPIRV unit tests
+    exit /b 1
+  )
+  if "%TEST_SPIRV_ONLY%"=="1" (
+    exit /b 0
+  )
+)
+rem End SPIRV change
 
 echo Copying binaries to test to %TEST_DIR%:
 
