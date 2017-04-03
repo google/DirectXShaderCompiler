e08b9fdca13c5ef49374be577dd7f9e455d6355b
Ehsan <ehsann@google.com>
2017-04-03 15:41:50 -0400

    Added Decoration, Type, and utils. (#16)
    * Added support for decorations and types.
    * Introduced clang::spirv::utils
    * Types and Decorations are unique in the context in which
      they are defined. Therefore, each context should track its
      own types, etc.
    * Added some unit tests. More to come.

--- tools/clang/include/clang/SPIRV/SPIRVContext.h
+++ tools/clang/include/clang/SPIRV/SPIRVContext.h
@@ -9,10 +9,27 @@
 #ifndef LLVM_CLANG_SPIRV_SPIRVCONTEXT_H
 #define LLVM_CLANG_SPIRV_SPIRVCONTEXT_H
 
+#include <unordered_map>
+
 #include "clang/Frontend/FrontendAction.h"
+#include "clang/SPIRV/Decoration.h"
+#include "clang/SPIRV/Type.h"
 
 namespace clang {
 namespace spirv {
+
+struct TypeHash {
+  std::size_t operator()(const Type &t) const {
+    // TODO: We could improve this hash function if necessary.
+    return std::hash<uint32_t>{}(static_cast<uint32_t>(t.getOpcode()));
+  }
+};
+struct DecorationHash {
+  std::size_t operator()(const Decoration &d) const {
+    // TODO: We could probably improve this hash function if needed.
+    return std::hash<uint32_t>{}(static_cast<uint32_t>(d.getValue()));
+  }
+};
 
 /// \brief A class for holding various data needed in SPIR-V codegen.
 /// It should outlive all SPIR-V codegen components that requires/allocates
@@ -27,16 +44,42 @@ public:
   SPIRVContext &operator=(const SPIRVContext &) = delete;
   SPIRVContext &operator=(SPIRVContext &&) = delete;
 
-  inline uint32_t GetNextId() const;
-  inline uint32_t TakeNextId();
+  inline uint32_t getNextId() const;
+  inline uint32_t takeNextId();
+
+  /// \brief Returns the <result-id> that defines the given Type. If the type
+  /// has not been defined, it will define and store its instruction.
+  uint32_t getResultIdForType(const Type *);
+
+  /// \brief Registers the existence of the given type in the current context,
+  /// and returns the unique Type pointer.
+  const Type *registerType(const Type &);
+
+  /// \brief Registers the existence of the given decoration in the current
+  /// context, and returns the unique Decoration pointer.
+  const Decoration *registerDecoration(const Decoration &);
 
 private:
-  uint32_t NextId;
+  using TypeSet = std::unordered_set<Type, TypeHash>;
+  using DecorationSet = std::unordered_set<Decoration, DecorationHash>;
+
+  uint32_t nextId;
+
+  /// \brief All the unique Decorations defined in the current context.
+  DecorationSet existingDecorations;
+
+  /// \brief All the unique types defined in the current context.
+  TypeSet existingTypes;
+
+  /// \brief Maps a given type to the <result-id> that is defined for
+  /// that type. If a Type* does not exist in the map, the type
+  /// is not yet defined and is not associated with a <result-id>.
+  std::unordered_map<const Type *, uint32_t> typeResultIdMap;
 };
 
-SPIRVContext::SPIRVContext() : NextId(1) {}
-uint32_t SPIRVContext::GetNextId() const { return NextId; }
-uint32_t SPIRVContext::TakeNextId() { return NextId++; }
+SPIRVContext::SPIRVContext() : nextId(1) {}
+uint32_t SPIRVContext::getNextId() const { return nextId; }
+uint32_t SPIRVContext::takeNextId() { return nextId++; }
 
 } // end namespace spirv
 } // end namespace clang
--- tools/clang/lib/SPIRV/InstBuilderManual.cpp
+++ tools/clang/lib/SPIRV/InstBuilderManual.cpp
@@ -8,6 +8,7 @@
 //===----------------------------------------------------------------------===//
 
 #include "clang/SPIRV/InstBuilder.h"
+#include "clang/SPIRV/Utils.h"
 
 #include "llvm/llvm_assert/assert.h"
 
@@ -15,26 +16,9 @@ namespace clang {
 namespace spirv {
 
 void InstBuilder::encodeString(std::string value) {
-  const size_t numChars = value.size();
-  const size_t oldSize = TheInst.size();
-  TheInst.resize(oldSize + (numChars / 4 + 1));
-  // Make sure all the bytes in the last word are zeros, in case we only
-  // write a partial word at the end.
-  TheInst.back() = 0;
-  // From the SPIR-V spec, literal string is
-  //
-  // A nul-terminated stream of characters consuming an integral number of
-  // words. The character set is Unicode in the UTF-8 encoding scheme. The UTF-8
-  // octets (8-bit bytes) are packed four per word, following the little-endian
-  // convention (i.e., the first octet is in the lowest-order 8 bits of the
-  // word). The final word contains the string's nul-termination character (0),
-  // and all contents past the end of the string in the final word are padded
-  // with 0.
-  //
-  // So the following works on little endian machines.
-  char *strDest = reinterpret_cast<char *>(&TheInst[oldSize]);
-  strncpy(strDest, value.c_str(), numChars);
-};
+  const auto& words = utils::encodeSPIRVString(value);
+  TheInst.insert(TheInst.end(), words.begin(), words.end());
+}
 
 } // end namespace spirv
 } // end namespace clang
