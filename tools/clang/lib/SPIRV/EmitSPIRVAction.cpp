ee5e1feef8d98f02d36e3ffd38e37bb0bbad1950
Lei Zhang <antiagainst@google.com>
2017-05-10 18:10:49 -0400

    [spirv] Handle basic block orders
    SPIR-V spec requires basic blocks to appear in an order satisfying
    the dominator-tree direction. Added a BlockReadableOrderVisitor
    class for visiting basic blocks in a human-readable order following
    the spec's requirements.
    
    Also fixed a bug in Constant::operator==(), which causes hashing
    weirdness.

--- tools/clang/lib/SPIRV/EmitSPIRVAction.cpp
+++ tools/clang/lib/SPIRV/EmitSPIRVAction.cpp
@@ -348,19 +348,24 @@ public:
     // We'll need the <label-id> for the then/else/merge block to do so.
     const bool hasElse = ifStmt->getElse() != nullptr;
     const uint32_t thenBB = theBuilder.createBasicBlock("if.true");
-    const uint32_t elseBB = hasElse ? theBuilder.createBasicBlock("if.false")
-                                    : theBuilder.createBasicBlock("if.merge");
-    const uint32_t mergeBB =
-        hasElse ? theBuilder.createBasicBlock("if.merge") : elseBB;
+    const uint32_t mergeBB = theBuilder.createBasicBlock("if.merge");
+    const uint32_t elseBB =
+        hasElse ? theBuilder.createBasicBlock("if.false") : mergeBB;
 
     // Create the branch instruction. This will end the current basic block.
     theBuilder.createConditionalBranch(condition, thenBB, elseBB, mergeBB);
+    theBuilder.addSuccessor(thenBB);
+    theBuilder.addSuccessor(elseBB);
+    // The current basic block has the OpSelectionMerge instruction. We need
+    // to record its merge target.
+    theBuilder.setMergeTarget(mergeBB);
 
     // Handle the then branch
     theBuilder.setInsertPoint(thenBB);
     doStmt(ifStmt->getThen());
     if (!theBuilder.isCurrentBasicBlockTerminated())
       theBuilder.createBranch(mergeBB);
+    theBuilder.addSuccessor(mergeBB);
 
     // Handle the else branch (if exists)
     if (hasElse) {
@@ -368,6 +373,7 @@ public:
       doStmt(ifStmt->getElse());
       if (!theBuilder.isCurrentBasicBlockTerminated())
         theBuilder.createBranch(mergeBB);
+      theBuilder.addSuccessor(mergeBB);
     }
 
     // From now on, we'll emit instructions into the merge block.
@@ -420,6 +426,7 @@ public:
       doStmt(initStmt);
     }
     theBuilder.createBranch(checkBB);
+    theBuilder.addSuccessor(checkBB);
 
     // Process the <check> block
     theBuilder.setInsertPoint(checkBB);
@@ -432,6 +439,12 @@ public:
     theBuilder.createConditionalBranch(condition, bodyBB,
                                        /*false branch*/ mergeBB,
                                        /*merge*/ mergeBB, continueBB);
+    theBuilder.addSuccessor(bodyBB);
+    theBuilder.addSuccessor(mergeBB);
+    // The current basic block has OpLoopMerge instruction. We need to set its
+    // continue and merge target.
+    theBuilder.setContinueTarget(continueBB);
+    theBuilder.setMergeTarget(mergeBB);
 
     // Process the <body> block
     theBuilder.setInsertPoint(bodyBB);
@@ -439,6 +452,7 @@ public:
       doStmt(body);
     }
     theBuilder.createBranch(continueBB);
+    theBuilder.addSuccessor(continueBB);
 
     // Process the <continue> block
     theBuilder.setInsertPoint(continueBB);
@@ -446,6 +460,7 @@ public:
       doExpr(cont);
     }
     theBuilder.createBranch(checkBB); // <continue> should jump back to header
+    theBuilder.addSuccessor(checkBB);
 
     // Set insertion point to the <merge> block for subsequent statements
     theBuilder.setInsertPoint(mergeBB);
@@ -497,10 +512,9 @@ public:
 
       if (expr->isConstantInitializer(astContext, false)) {
         return theBuilder.getConstantComposite(resultType, constituents);
+      } else {
+        return theBuilder.createCompositeConstruct(resultType, constituents);
       }
-      // TODO: use OpCompositeConstruct for non-constant initializer lists.
-      emitError("Non-const initializer lists are currently not supported.");
-      return 0;
     } else if (auto *boolLiteral = dyn_cast<CXXBoolLiteralExpr>(expr)) {
       const bool value = boolLiteral->getValue();
       return theBuilder.getConstantBool(value);
@@ -606,6 +620,15 @@ public:
         emitError("Integral cast is not supported yet");
         return 0;
       }
+    }
+    case CastKind::CK_FloatingCast: {
+      Expr::EvalResult evalResult;
+      if (expr->EvaluateAsRValue(evalResult, astContext) &&
+          !evalResult.HasSideEffects) {
+        return translateAPFloat(evalResult.Val.getFloat(), toType);
+      }
+      emitError("floating cast unimplemented");
+      return 0;
     }
     case CastKind::CK_LValueToRValue: {
       const uint32_t fromValue = doExpr(subExpr);
