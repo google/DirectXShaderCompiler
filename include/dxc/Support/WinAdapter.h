e8086cda94d15a1c7977aec355ba19657b3683e5
Greg Roth <goyoroth@gmail.com>
2018-06-21 14:45:13 -0600

    [linux-port] add ERROR_DLL_INIT_FAILED

--- include/dxc/Support/WinAdapter.h
+++ include/dxc/Support/WinAdapter.h
@@ -105,6 +105,9 @@
 #define ERROR_IO_DEVICE EIO
 #define ERROR_INVALID_HANDLE EBADF
 
+// Error returned explicitly. Windows value used.
+#define ERROR_DLL_INIT_FAILED 0x45A
+
 // Used by HRESULT <--> WIN32 error code conversion
 #define SEVERITY_ERROR 1
 #define FACILITY_WIN32 7
