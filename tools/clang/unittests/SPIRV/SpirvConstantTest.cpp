5e58f7acd152e766742d6c9598bb7d46367a7e38
Ehsan Nasiri <ehsannas@gmail.com>
2018-10-26 13:18:36 -0400

    Update unit test.

--- tools/clang/unittests/SPIRV/SpirvConstantTest.cpp
+++ tools/clang/unittests/SPIRV/SpirvConstantTest.cpp
@@ -10,26 +10,30 @@
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 
+#include "clang/SPIRV/SPIRVContext.h"
 #include "clang/SPIRV/SpirvInstruction.h"
 
 namespace {
 using namespace clang::spirv;
 
 TEST(SpirvConstant, Float16) {
+  SpirvContext ctx;
   const uint16_t f16 = 12;
-  SpirvConstantFloat constant(f16, {}, 0, {});
+  SpirvConstantFloat constant(ctx.getFloatType(16), f16);
   EXPECT_EQ(f16, constant.getValue16());
 }
 
 TEST(SpirvConstant, Float32) {
+  SpirvContext ctx;
   const float f32 = 1.5;
-  SpirvConstantFloat constant(f32, {}, 0, {});
+  SpirvConstantFloat constant(ctx.getFloatType(32), f32);
   EXPECT_EQ(f32, constant.getValue32());
 }
 
 TEST(SpirvConstant, Float64) {
+  SpirvContext ctx;
   const double f64 = 3.14;
-  SpirvConstantFloat constant(f64, {}, 0, {});
+  SpirvConstantFloat constant(ctx.getFloatType(64), f64);
   EXPECT_EQ(f64, constant.getValue64());
 }
 
