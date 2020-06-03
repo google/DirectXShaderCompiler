890dbbe6466384a451a4be846a75e180123435d5
Jaebaek Seo <jaebaek@google.com>
2020-06-03 12:01:42 -0400

    Fix test failure

--- tools/clang/test/CodeGenSPIRV/rich.debug.type.array-from-same-type.hlsl
+++ tools/clang/test/CodeGenSPIRV/rich.debug.type.array-from-same-type.hlsl
@@ -9,15 +9,15 @@ struct UBO
 
 cbuffer ubo : register(b0) { UBO ubo; }
 
-// CHECK: [[fooName:%\d+]] = OpString "foo"
-// CHECK:   [[aName:%\d+]] = OpString "a"
 // CHECK:   [[bName:%\d+]] = OpString "b"
+// CHECK:   [[aName:%\d+]] = OpString "a"
+// CHECK: [[fooName:%\d+]] = OpString "foo"
 
-// CHECK: [[matf4v4:%\d+]] = OpExtInst %void [[ext:%\d+]] DebugTypeArray [[dbg_f:%\d+]] %uint_4 %uint_4
-// CHECK: [[matf4v4_arr3:%\d+]] = OpExtInst %void [[ext]] DebugTypeArray [[dbg_f]] %uint_3 %uint_4 %uint_4
-// CHECK: OpExtInst %void [[ext]] DebugLocalVariable [[fooName]] [[matf4v4]]
-// CHECK: OpExtInst %void [[ext]] DebugTypeMember [[aName]] [[matf4v4]]
+// CHECK: [[matf4v4_arr3:%\d+]] = OpExtInst %void [[ext:%\d+]] DebugTypeArray [[dbg_f:%\d+]] %uint_3 %uint_4 %uint_4
 // CHECK: OpExtInst %void [[ext]] DebugTypeMember [[bName]] [[matf4v4_arr3]]
+// CHECK: [[matf4v4:%\d+]] = OpExtInst %void [[ext]] DebugTypeArray [[dbg_f]] %uint_4 %uint_4
+// CHECK: OpExtInst %void [[ext]] DebugTypeMember [[aName]] [[matf4v4]]
+// CHECK: OpExtInst %void [[ext]] DebugLocalVariable [[fooName]] [[matf4v4]]
 
 void main() {
   float4x4 foo = ubo.a;
